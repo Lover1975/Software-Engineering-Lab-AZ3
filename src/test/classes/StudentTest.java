ee97b279268c967fcf6463316f2073876670b6e4
Lover1975 <drjoenh1983@gmail.com>
2024-07-28 06:11:48 -0700

    Revert "Add unit tests for displayBooks and toString methods in Student class to increase its coverage to 100 percent."
    This reverts commit 845ea57d2a02919572b64e0f8071913678c1d1c1.

--- src/test/classes/StudentTest.java
+++ src/test/classes/StudentTest.java
@@ -6,10 +6,7 @@ import main.classes.Student;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import main.classes.Book;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,10 +16,6 @@ public class StudentTest {
     private Student student1;
     private Student student2;
     private Student student3;
-    private Student student4;
-
-    private Book book1;
-    private Book book2;
 
     @BeforeEach
     public void setUp() {
@@ -30,15 +23,9 @@ public class StudentTest {
         student1 = new Student("Mahdi", 1);
         student2 = new Student("Ali", 2);
         student3 = new Student("Hasan", 3);
-        student4 = new Student("Mohsen", 4);
         library.addStudent(student1);
         library.addStudent(student2);
         library.addStudent(student3);
-        library.addStudent(student4);
-        book1 = new Book("The Alchemist", "Paulo Coelho", 2);
-        book2 = new Book("The Da Vinci Code", "Dan Brown", 3);
-        student4.addBook(book1);
-        student4.addBook(book2);
     }
 
     @Test
@@ -97,21 +84,5 @@ public class StudentTest {
         assertNull(result, "The method should return null for AUTHOR search type.");
     }
 
-    @Test
-    public void testDisplayBooks() {
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        student4.displayBooks();
-        String expectedOutput = "Mohsen|4 has these books:" + System.lineSeparator() +
-                "The Alchemist by Paulo Coelho" + System.lineSeparator() +
-                "The Da Vinci Code by Dan Brown" + System.lineSeparator();
-        assertEquals(expectedOutput, outContent.toString());
-        System.setOut(System.out);
-    }
-
-    @Test
-    public void testToString() {
-        assertEquals("Mohsen|4", student4.toString());
-    }
 
 }
