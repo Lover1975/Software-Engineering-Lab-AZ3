3e5456a238d1d04ab42216853be5aa7caf89e609
Lover1975 <drjoenh1983@gmail.com>
2024-07-28 06:16:27 -0700

    Add tests for displayBooks and toString methods, increasing coverage for Student and Book classes

--- src/test/classes/StudentTest.java
+++ src/test/classes/StudentTest.java
@@ -1,12 +1,15 @@
 package test.classes;
 
 import main.classes.Library;
+import main.classes.Book;
 import main.classes.SearchByType;
 import main.classes.Student;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +19,9 @@ public class StudentTest {
     private Student student1;
     private Student student2;
     private Student student3;
+    private Student student4;
+    private Book book1;
+    private Book book2;
 
     @BeforeEach
     public void setUp() {
@@ -23,9 +29,15 @@ public class StudentTest {
         student1 = new Student("Mahdi", 1);
         student2 = new Student("Ali", 2);
         student3 = new Student("Hasan", 3);
+        student4 = new Student("Mohsen", 4);
         library.addStudent(student1);
         library.addStudent(student2);
         library.addStudent(student3);
+        library.addStudent(student4);
+        book1 = new Book("The Alchemist", "Paulo Coelho", 2);
+        book2 = new Book("The Da Vinci Code", "Dan Brown", 3);
+        student4.addBook(book1);
+        student4.addBook(book2);
     }
 
     @Test
@@ -82,6 +94,23 @@ public class StudentTest {
         authors.add("Some Author");
         List<Student> result = library.searchStudents(SearchByType.AUTHOR, authors);
         assertNull(result, "The method should return null for AUTHOR search type.");
+    }
+
+    @Test
+    public void testDisplayBooks() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        student4.displayBooks();
+        String expectedOutput = "Mohsen|4 has these books:" + System.lineSeparator() +
+                "The Alchemist by Paulo Coelho" + System.lineSeparator() +
+                "The Da Vinci Code by Dan Brown" + System.lineSeparator();
+        assertEquals(expectedOutput, outContent.toString());
+        System.setOut(System.out);
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("Mohsen|4", student4.toString());
     }
 
 
